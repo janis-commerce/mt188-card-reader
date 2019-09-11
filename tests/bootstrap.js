5f88472027ed25dbccc7ee1f452b638e4de1b40d
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2019-09-11 18:14:04 -0300

    Test drive

---
+++ tests/bootstrap.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const logger = require('lllog')();
+
+logger.setMinLevel('none');
--- tests/mt188-card-reader-test.js
+++ tests/mt188-card-reader-test.js
@@ -1,15 +1,102 @@
 'use strict';
 
-// const assert = require('assert');
+const assert = require('assert');
+const sinon = require('sinon');
+const MT188Driver = require('mt188-card-reader-windows-driver');
 
-// const sandbox = require('sinon').createSandbox();
-
-// const MT188CardReader = require('./../index');
-
-// const MT188CardReaderError = require('./../lib/mt188-card-reader-error');
+const { MT188CardReader, MT188CardReaderError } = require('../lib');
 
 describe('MT188CardReader', () => {
 
-	// your tests here...
+	describe('Get PAN', () => {
 
+		it('Should throw if devices cannot be listed', async () => {
+
+			sinon.stub(MT188Driver, 'selectDevice')
+				.throws(Error, 'Some error');
+
+			const cardReader = new MT188CardReader();
+			await assert.rejects(() => cardReader.getPAN(), MT188CardReaderError);
+		});
+
+		it('Should throw if no devices are found', async () => {
+
+			sinon.stub(MT188Driver, 'selectDevice')
+				.returns([]);
+
+			const cardReader = new MT188CardReader();
+			await assert.rejects(() => cardReader.getPAN(), MT188CardReaderError);
+		});
+
+		it('Should throw if device cannot be opened', async () => {
+
+			sinon.stub(MT188Driver, 'selectDevice')
+				.returns(['Some Device']);
+
+			sinon.stub(MT188Driver, 'open')
+				.throws(Error, 'Some error');
+
+			const cardReader = new MT188CardReader();
+			await assert.rejects(() => cardReader.getPAN(), MT188CardReaderError);
+		});
+
+		it('Should throw if card type cannot be fetched', async () => {
+
+			const atr = '8801015f2d0865737074656e66729f1101019000';
+
+			sinon.stub(MT188Driver, 'selectDevice')
+				.returns(['Some Device']);
+
+			sinon.stub(MT188Driver, 'open')
+				.returns(atr);
+
+			sinon.stub(MT188Driver, 'getCardType')
+				.throws(Error, 'Some error');
+
+			const cardReader = new MT188CardReader();
+			await assert.rejects(() => cardReader.getPAN(), MT188CardReaderError);
+		});
+
+		it('Should throw if card PAN cannot be fetched', async () => {
+
+			const atr = '8801015f2d0865737074656e66729f1101019000';
+			const cardType = MT188Driver.CardType.ContactIC;
+
+			sinon.stub(MT188Driver, 'selectDevice')
+				.returns(['Some Device']);
+
+			sinon.stub(MT188Driver, 'open')
+				.returns(atr);
+
+			sinon.stub(MT188Driver, 'getCardType')
+				.returns(cardType);
+
+			sinon.stub(MT188Driver, 'getPANFromCardType')
+				.throws(Error, 'Some error');
+
+			const cardReader = new MT188CardReader();
+			await assert.rejects(() => cardReader.getPAN(), MT188CardReaderError);
+		});
+
+		it('Should throw if no card is present', async () => {
+
+			const atr = '8801015f2d0865737074656e66729f1101019000';
+			const cardType = MT188Driver.CardType.NoCardInside;
+
+			sinon.stub(MT188Driver, 'selectDevice')
+				.returns(['Some Device']);
+
+			sinon.stub(MT188Driver, 'open')
+				.returns(atr);
+
+			sinon.stub(MT188Driver, 'getCardType')
+				.returns(cardType);
+
+			const cardReader = new MT188CardReader();
+			await assert.rejects(() => cardReader.getPAN(), {
+				name: 'MT188CardReaderError',
+				message: 'No card present'
+			});
+		});
+	});
 });
