a7e7c266fa2b36ef6fd0a9506e7d65a91e557c4a
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2019-09-12 20:06:17 -0300

    getPANWithRetries added to reader's API

--- examples/chip.js
+++ examples/chip.js
@@ -10,37 +10,15 @@ const { MT188CardReader } = require('../lib');
 
 const cardReader = new MT188CardReader();
 
-const MAX_ATTEMPTS = 20;
-let attempts = 0;
+cardReader.getPANWithRetries({ contactless: false })
+	.then(pan => {
 
-const intervalId = setInterval(() => {
-
-	attempts++;
-
-	cardReader.getPAN({ contactless: false })
-		.then(pan => {
-
-			logger.info();
-
-			if(pan) {
-				logger.info('Card data found!');
-				// eslint-disable-next-line no-console
-				console.log({
-					...pan,
-					cardNumber: pan.cardNumber.replace(/^([0-9]{6})[0-9]{6}([0-9]{4})/, '$1******$2')
-				});
-				clearInterval(intervalId);
-			} else {
-				if(attempts >= MAX_ATTEMPTS)
-					clearInterval(intervalId);
-
-				logger.error('Could not get card data.');
-			}
-		})
-		.catch(e => {
-			logger.error(e.message);
-
-			if(attempts >= MAX_ATTEMPTS)
-				clearInterval(intervalId);
+		// eslint-disable-next-line no-console
+		console.log({
+			...pan,
+			cardNumber: pan.cardNumber.replace(/^([0-9]{6})[0-9]{6}([0-9]{4})/, '$1******$2')
 		});
-}, 1000);
+	})
+	.catch(e => {
+		logger.error(e.message);
+	});
